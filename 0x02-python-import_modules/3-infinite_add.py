218ba610db857a2ef912ae2fbd40129d7eccf321
Nathan <frevnjengamungai@gmail.com>
2022-12-14 05:44:17 -0800

    error handling

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-argc = len(argv)
-result = 0
-if argc == 1:
-    print("{:d}".format(result))
-else:
-    for i in range(1, argc):
-        result += int(argv[i])
-    print("{:d}".format(result))
+
+    count = len(argv)
+    size = 0
+
+    if count == 1:
+        print("{:d}".format(size))
+    else:
+        for i in range(1, count):
+            size = size + int(argv[i])
+
+        print("{:d}".format(size))
