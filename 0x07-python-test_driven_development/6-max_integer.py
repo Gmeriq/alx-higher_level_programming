ae2b57a2f5e138935bd6b41dc8b686981b5809ca
Nate <49339539+Gmeriq@users.noreply.github.com>
2023-01-09 18:15:21 +0300

    add 6-max_integer.py

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
+"""Unittest for max_integer([..])
 """
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
+class TestMaxInteger(unittest.TestCase):
+    def test_empty_list(self):
+        """
+        Test the max_integer function with an empty list.
+        """
+        self.assertIsNone(max_integer([]))
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+    def test_one_item_list(self):
+        """
+        Test the max_integer function with a list containing one item.
+        """
+        self.assertEqual(max_integer([1]), 1)
+
+    def test_three_items_list(self):
+        """
+        Test the max_integer function with a list containing three items.
+        """
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+        self.assertEqual(max_integer([2, 3, 1]), 3)
+        self.assertEqual(max_integer([3, 1, 2]), 3)
+        self.assertEqual(max_integer([3, 2, 1]), 3)
+    def test_floats(self):
+        """Test a list of floats."""
+        floats = [1.53, 6.33, -9.123, 15.2, 6.0]
+        self.assertEqual(max_integer(floats), 15.2)
