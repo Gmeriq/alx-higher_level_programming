6937d3e48410e1d329ae387ab8980c00cf24772a
Nate <49339539+Gmeriq@users.noreply.github.com>
2023-01-09 18:37:18 +0300

    error handling

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+
+"""This module contains a function that multiplies two matrices"""
+
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    res = np.dot(m_a, m_b)
-    return res
+    """Return the multiplication of two matrices.
+    Args:
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
+    """
+
+    return (np.matmul(m_a, m_b))
