5f3906b2724be279b6ca3e08b0343577eb54c0a5
Nate <49339539+Gmeriq@users.noreply.github.com>
2023-01-10 17:36:50 +0300

    error handling.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Adds all arguments to list"""
+"""This module adds all arguments to a Python list and save them to a file."""
 
+
+import sys
 
 if __name__ == "__main__":
-    from sys import argv
-    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-    data = []
     try:
-        data = load_from_json_file("add_items.json")
-        for item in argv[1:]:
-            data.append(item)
-        save_to_json_file(data, "add_item.json")
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        for item in argv[1:]:
-            data.append(item)
-        save_to_json_file(data, "add_item.json")
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
