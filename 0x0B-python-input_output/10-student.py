1eec7ed642c38c8af90163899bae1a2482e2157b
Nate <49339539+Gmeriq@users.noreply.github.com>
2023-01-10 17:40:20 +0300

    Handling errors.

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-"""Defines student class"""
+"""This module defines a class Student"""
 
 
 class Student:
-    """Student object"""
-
-    first_name = ""
-    last_name = ""
-    age = 0
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializing instance"""
-
+        """Initializes a new Student
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return dictionary representation of Student"""
-        if isinstance(attrs, list) and\
-            all(isinstance(item, str) for item in attrs):
-            result = {}
-            for i in attrs:
-                try:
-                    result[i] = self.__dict__[i]
-                except Exception:
-                    pass
-            return result
+        """Get a dictionary representation of the Student
+        If attrs is a list of strings, represents only those attributes
+        included in the list
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
