ceb316ac32fbaab22f83ab1d819d205688b9ad9c
Nate <49339539+Gmeriq@users.noreply.github.com>
2023-01-16 13:41:20 +0300

    Error handling

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
+def print_reversed_list_integer(my_list=[]):
     if not my_list:
         return None
-    my_list.reverse()
-    for i in range(0, len(my_list)):
-        print('{:d}'.format(my_list[i]))
+    for i in reversed(range(len(my_list))):
+        print("{:d}".format(my_list[i]))
