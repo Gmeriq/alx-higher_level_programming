e8810ddfa135f320b634f46b05da6a853db11e43
Nathan <frevnjengamungai@gmail.com>
2023-01-16 03:32:25 -0800

    correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = ''
-    for i in my_string:
-        if i != 'c' and i != 'C':
-            new += i
+    new = my_string.copy()
+    for i in ne:
+        if i == 'c' and i == 'C':
+            del(i)
 
     return new
