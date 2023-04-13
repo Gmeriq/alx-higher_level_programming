803aea5c10f40b10d3e1848f27c3446cba8d08b6
Nathan Njenga <frevnjengamungai@gmail.com>
2023-04-13 15:45:14 +0300

    output check

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,19 +1,14 @@
 #!/usr/bin/node
 
-//Prints arguments in a format
+// Prints arguments in a format
 
-const none = "udefined";
+const none = 'undefined';
 const argv = process.argv;
 
-if (!argv[2])
-{
-    console.log('${none} is ${none}');
-}
-else if (!argv[3])
-{
-    console.log("${argv[2]} is ${none}");
-}
-else
-{
-    console.log("${argv[2]} is ${argv[3]}");
+if (!argv[2]) {
+  console.log(`${none} is ${none}`);
+} else if (!argv[3]) {
+  console.log(`${argv[2]} is ${none}`);
+} else {
+  console.log(`${argv[2]} is ${argv[3]}`);
 }
