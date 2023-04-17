64d5208f6823d526bbc602162a9ad83beb229def
Nathan Njenga <frevnjengamungai@gmail.com>
2023-04-17 11:47:52 +0300

    error handling

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+
 """ Connect to database """
+
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], 
-                     passwd=argv[2], db=argv[3], charset="utf8")
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
 
-curs = db.cursor()
-curs.execute('SELECT* FROM states ORDER BY id ASC')
-states = curs.fetchall()
-for state in states:
-    print(state)
+    curs = db.cursor()
+    curs.execute('SELECT* FROM states ORDER BY id ASC')
+    states = curs.fetchall()
+    for state in states:
+        print(state)
 
-curs.close()
-db.close()
+    curs.close()
+    db.close()
