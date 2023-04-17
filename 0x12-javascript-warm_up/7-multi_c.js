a50383dd81d080594d9e595a2cfa85121176044a
Nathan Njenga <frevnjengamungai@gmail.com>
2023-04-17 11:35:50 +0300

    Add 0-select_states.py file

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,15 +5,11 @@
 const argv = process.argv[2];
 let i = 0;
 
-if (!isNaN(argv))
-{
-    while (i < argv)
-    {
-        console.log("C is fun");
-        i++;
-    }
-}
-else
-{
-    console.log("Missing number of occurrences");
+if (!isNaN(argv)) {
+  while (i < argv) {
+    console.log('C is fun');
+    i++;
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
