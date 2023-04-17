34317a09b337522bcb24f3af0853ac161adecb99
Nathan Njenga <frevnjengamungai@gmail.com>
2023-04-17 13:39:26 +0300

    change mode

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""List all cities from database"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    curs = db.cursor()
+    curs.execute("SELECT cities.id, cities.name, states.name {} {} {}".format(
+        "FROM cities",
+        "LEFT OUTER JOIN states ON cities.states_id=states.id",
+        "ORDER BYcities.id ASC"))
+
+    data = curs.fetchall()
+    for row in data:
+        print(row)
+    curs.close()
+    db.close()
