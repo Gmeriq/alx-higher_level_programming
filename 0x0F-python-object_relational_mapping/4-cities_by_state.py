d1f12d26d5a6daf4852a7eda8e25c21ac1479fd3
Nathan Njenga <frevnjengamungai@gmail.com>
2023-04-17 13:52:05 +0300

    errors

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,11 +9,12 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     curs = db.cursor()
-    curs.execute("SELECT cities.id, cities.name, states.name {} {} {}".format(
-        "FROM cities",
-        "LEFT OUTER JOIN states ON cities.states_id=states.id",
-        "ORDER BY cities.id ASC"))
-
+    query = "SELECT cities.id, cities.name, states.name {} {} {}".format(
+            "FROM cities",
+            "LEFT OUTER JOIN states ON cities.states_id=states.id",
+            "ORDER BY cities.id ASC")
+    
+    curs.execute(query)
     data = curs.fetchall()
     for row in data:
         print(row)
