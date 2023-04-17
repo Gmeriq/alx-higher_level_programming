4902514e2cb4ace8717271ef4b16e868379153cf
Nathan Njenga <frevnjengamungai@gmail.com>
2023-04-17 12:17:36 +0300

    Add 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""List states starting with N"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    curs = database.cursor()
+    query = 'SELECT * FROM states WHERE name LIKE BINARY"N%" ORDER BY id ASC'
+    curs.execute(query)
+    nstates = curs.fetchall()
+
+    for state in nstates:
+        print(state)
+
+    curs.close()
+    db.close()
