daf46bfbf0afbec44ea5a267fdeef70698674d02
Nathan Njenga <frevnjengamungai@gmail.com>
2023-04-17 14:59:19 +0300

    fix errors

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-
-"""List all cities from database
+""" Script that lists all cities from the database hbtn_0e_4_usa
 """
-
 import MySQLdb
 from sys import argv
 
-if __name__ = "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
+
+def main():
+    database = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                               passwd=argv[2], db=argv[3], charset="utf8")
+    cur = database.cursor()
     query = "SELECT cities.id, cities.name, states.name {} {} {}".format(
             "FROM cities",
             "LEFT OUTER JOIN states ON cities.state_id=states.id",
@@ -19,4 +18,8 @@ if __name__ = "__main__":
     for row in data:
         print(row)
     cur.close()
-    db.close()
+    database.close()
+
+
+if __name__ == "__main__":
+    main()
